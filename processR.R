f19a0ac9bf166bae7135afb1a873a2a44286fbde
prabhushreya <54602344+prabhushreya@users.noreply.github.com>
2019-10-19 00:24:50 -0400

    Delete processR.R

--- processR.R
+++
@@ -1,9 +0,0 @@
-lib = read.csv("./public_libraries.csv", stringsAsFactors = F)
-head(lib)
-#View(df)
-
-#class(df$Longitude)
-
-sample_data <- lib[c(1:1000),]
-saveRDS(sample_data, "./sample_lib.rds")
-
